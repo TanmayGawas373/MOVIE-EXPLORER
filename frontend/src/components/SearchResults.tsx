f37e4bfb3df6a9d3f85e25e9f03cfb041aa2fc96
Tanmai Gawas <gawastanmay373@gmail.com>
2025-07-22 15:16:28 +0530

    image options updated

--- frontend/src/components/SearchResults.tsx
+++ frontend/src/components/SearchResults.tsx
@@ -13,7 +13,7 @@ const SearchResults = () => {
             
                 <h3>{movie['#TITLE']} ({movie['#YEAR']})</h3>
             <p>IMDb ID: {movie['#IMDB_ID']}</p>
-            <img src={movie['#IMG_POSTER']} alt={`${movie['#TITLE']} poster`} className='h-50 w-50'/>
+            <img src={movie['#IMG_POSTER']||'https://www.google.com/imgres?q=movie%20camera%20drawing&imgurl=https%3A%2F%2Fwww.shutterstock.com%2Fimage-vector%2Fretro-movie-camera-film-strip-600w-2160752449.jpg&imgrefurl=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fvintage-drawings-old-movie-camera&docid=eS-ZWDzrykkX1M&tbnid=rYjse9aIqhs8YM&vet=12ahUKEwjZgZ20k9COAxXQia8BHb8wEhEQM3oECBEQAA..i&w=600&h=583&hcb=2&ved=2ahUKEwjZgZ20k9COAxXQia8BHb8wEhEQM3oECBEQAA'} alt={`${movie['#TITLE']} poster`} className='h-50 w-50'/>
             
           </div>
         ))
